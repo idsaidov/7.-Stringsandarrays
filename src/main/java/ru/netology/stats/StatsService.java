343d651a38801dd28653b2ab11cd74e449d64ff9
idsaidov <id.saidov@yandex.ru>
2022-08-21 21:55:45 +0300

    Update

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -2,8 +2,7 @@ package ru.netology.stats;
 
 public class StatsService {
 
-
-    public int maxSales(long[] sales) {
+    public int whichWasThePeakSales(long[] sales) {
         int month = 0;
         int maxMonth = 0;
         for (long sale : sales) {
@@ -15,7 +14,7 @@ public class StatsService {
         return maxMonth + 1;
     }
 
-    public int minSales(long[] sales) {
+    public int whichWasTheMinSales(long[] sales) {
         int month = 0;
         int minMonth = 0;
         for (long sale : sales) {
@@ -26,7 +25,38 @@ public class StatsService {
         }
         return minMonth + 1;
     }
+    public long sumAllSales(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
 
+    public long  averageAmountSalesInMonth(long[] sales) {
+        long average = sumAllSales(sales) / sales.length;
+        return average;
+    }
+    public int inWhichSalesWereBelowAverage(long[] sales) {
+        int months = 0;
+        long average = averageAmountSalesInMonth(sales);
+        for (long sale : sales) {
+            if (sale < average) {
+                months++;
+            }
+        }
+        return months;
+    }
 
+    public int inWhichSalesWereAboveAverage(long[] sales) {
+        int months = 0;
+        long average = averageAmountSalesInMonth(sales);
+        for (long sale : sales) {
+            if (sale > average) {
+                months++;
+            }
+        }
+        return months;
+    }
 
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -6,13 +6,12 @@ import org.junit.jupiter.api.Test;
 class StatsServiceTest {
 
     long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-
     @Test
     void shouldFindMaxSalesMonth() {
         StatsService service = new StatsService();
 
         long expected = 8;
-        long actual = service.maxSales(sales);
+        long actual = service.whichWasThePeakSales(sales);
         Assertions.assertEquals(expected, actual);
     }
 
@@ -21,8 +20,42 @@ class StatsServiceTest {
         StatsService service = new StatsService();
 
         long expected = 9;
-        long actual = service.minSales(sales);
+        long actual = service.whichWasTheMinSales(sales);
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    void shouldCalculateSumAllSales() {
+        StatsService service = new StatsService();
+
+        long expected = 180;
+        long actual = service.sumAllSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateAverageSales() {
+        StatsService service = new StatsService();
+
+        long expected = 15;
+        long actual = service.averageAmountSalesInMonth(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCountSalesBelowAvgMonths() {
+        StatsService service = new StatsService();
+
+        long expected = 5;
+        long actual = service.inWhichSalesWereBelowAverage(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCountSalesAboveAvgMonths() {
+        StatsService service = new StatsService();
+
+        long expected = 5;
+        long actual = service.inWhichSalesWereAboveAverage(sales);
+        Assertions.assertEquals(expected, actual);
+    }
 }
